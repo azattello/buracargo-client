c5b92e63b1714ebbea49db830ef310f74bfc2a2b
Abdykali Azat <azat.abdykalli@gmail.com>
2024-07-17 18:25:51 +0500

    first commit

---
+++ src/config.js
@@ -0,0 +1,11 @@
+// config.js
+const config = {
+    // apiUrl: 'http://localhost:3001',
+    apiUrl: 'https://buracargo.kz:3001',
+    
+    nameCargo: 'BURA CARGO'
+  };
+  
+  
+export default config;
+  
